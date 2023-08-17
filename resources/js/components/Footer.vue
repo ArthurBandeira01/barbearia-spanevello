726a22d1e5da933e870619c0fe5c8c6127627210
Arthur Bandeira <arthurbandeirafc@gmail.com>
2023-08-17 18:45:10 -0300

    Redes sociais no rodapé

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -8,6 +8,23 @@
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3454.648827983785!2d-52.91461587444725!3d-30.018238374936733!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9504a74f9642e943%3A0x455b09d66b4cad9d!2sBarbearia%20Spanevello!5e0!3m2!1spt-BR!2sbr!4v1692283321852!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
    <footer class="py-3">
+    <div class="d-flex justify-content-center mb-3">
+        <div class="socials">
+            <a href="https://www.facebook.com/barbeariaspanevello">
+                <i class="fab fa-facebook"></i>
+            </a>
+        </div>
+        <div class="socials ms-3">
+            <a href="https://www.instagram.com/barbearia_spanevello/">
+                <i class="fab fa-instagram"></i>
+            </a>
+        </div>
+        <div class="socials ms-3">
+            <a href="https://wa.me/5551996948106?text=Ol%C3%A1%2C+venho+a+partir+do+site+para+marcar+um+hor%C3%A1rio+%3A%29">
+                <i class="fab fa-whatsapp"></i>
+            </a>
+        </div>
+    </div>
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
       <li class="nav-item"><a href="" class="nav-link px-2 text-body-secondary">Início</a></li>
       <li class="nav-item"><a href="#about" class="nav-link px-2 text-body-secondary">Sobre</a></li>
@@ -58,7 +75,10 @@ footer{
     background-color: var(--escuro);
     color: var(--dourado-claro);
 }
-
+.socials a i{
+    color: var(--dourado-claro);
+    font-size: 30px;
+}
 @keyframes pulse {
     0% {
         transform: scale(1);
